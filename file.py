c55d87b8bc2b799fe971521127e3d02f91096974
AlexPetrushkova <midnight_sprinter@hotmail.com>
2024-07-10 11:51:22 +0300

    Changed py file

--- file.py
+++ file.py
@@ -1,6 +1,6 @@
-import strings
+import string
 import random
 
-chars = strings.digits + strings.letters
+chars = string.digits + string.ascii_letters + string.punctuation
 password = "".join([random.choice(chars) for _ in range(10)])
 print(f"This is your generated password {password}")
